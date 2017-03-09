94f83ff0437c6a9e0c2adec3c8cd00d9b68c7c4d
djchris261 <djchris261@hotmail.com>
2017-03-09 14:48:45 +0100

    Main file

--- state_space.py
+++ state_space.py
@@ -11,42 +11,39 @@ Created on Wed Mar 08 10:47:01 2017
 @author: Chris
 """
 
-from Cit_par import *
 import numpy as np
 import control
-import matplotlib.pyplot
+ 
 
 
-#Variables that need to be calcualted
-V  = V0 
-Cl = 0.8 
-
-#SYMETRIC FLIGHT CONDITIONS
-
-CS1 = np.matrix( [[-2*muc*c/V/V,0,0,0],    [0,(CZa-2*muc)*c/V,0,0],[0,0,-c/V,0],[0,Cmadot*c/V,0,-2*muc*KY2*c*c/V/V] ])
-CS2 = np.matrix([ [CXu/V,CXa,CZq,CXu*c/V ],[CZu/V, CZa, -CX0 , c/V*(CZq+2*muc) ], [ 0,0,0,c/V ], [ Cmu/V,Cma,0,Cmq*c/V ]])
-CS3 = np.matrix( [[CXde],[CZde],[0],[Cmde]])
-
-A_s = -CS1**(-1)*CS2
-B_s = -CS1**(-1)*CS3
-C_s = np.matrix([ [1,0,0,0 ],[0,1,0,0 ], [ 0,0,1,0 ], [ 0,0,0,1 ]])
-D_s = np.matrix( [[0],[0],[0],[0]])
-
-sys_sym = control.StateSpace(A_s,B_s,C_s,D_s)
-
-
-#ASYMMETRIC FLIGHT CONDITIONS
-
-CA1 = np.matrix( [[(CYbdot-2*mub)*b/V,0,0,0],    [0,-b/2/V,0,0],[0,0,-4*mub*KX2*b*b/2/V/V,4*mub*KXZ*b*b/2/V/V],[Cnb*b/V,0,4*mub*KXZ*b*b/2/V/V,-4*mub*KZ2*b*b/2/V/V] ])
-CA2 = np.matrix([ [CYb,Cl,CYp*b/V/2,(CYr-4*mub)*b/2/V ],[0, 0, b/V/2 , 0 ], [ Clb,0,Clp*b/2/V,Clr*b/2/V ], [ Cnb,0,Cnp*b/2/V,Cnr*b/2/V ]])
-CA3 = np.matrix( [[CYda,CYdr],[0,0],[Clda,Cldr],[Cnda,Cndr]])
-
-A_a = -CA1**(-1)*CA2
-B_a = -CA1**(-1)*CA3
-C_a = np.matrix([ [1,0,0,0 ],[0,1,0,0 ], [ 0,0,1,0 ], [ 0,0,0,1 ]])
-D_a = np.matrix( [[0,0],[0,0],[0,0],[0,0]])
-
-sys_asym = control.StateSpace(A_a,B_a,C_a,D_a)
-
-
-
+def state_space2(Clr,Clda,muc,c,V,CZa,Cmadot,KY2,CXu,CXa,CZq,CZu,CX0,Cmu,Cma,Cmq,CXde,CZde,Cmde,CYbdot,b,mub,KX2,KXZ,KZ2,CYb,Cl,CYp,CYr,Clb,Clp,Cnb,Cnp,Cnr,CYda,Cldr,Cnda,Cndr,CYdr):
+    #SYMETRIC FLIGHT CONDITIONS
+    
+    CS1 = np.matrix( [[-2*muc*c/V/V,0,0,0],    [0,(CZa-2*muc)*c/V,0,0],[0,0,-c/V,0],[0,Cmadot*c/V,0,-2*muc*KY2*c*c/V/V] ])
+    CS2 = np.matrix([ [CXu/V,CXa,CZq,CXu*c/V ],[CZu/V, CZa, -CX0 , c/V*(CZq+2*muc) ], [ 0,0,0,c/V ], [ Cmu/V,Cma,0,Cmq*c/V ]])
+    CS3 = np.matrix( [[CXde],[CZde],[0],[Cmde]])
+    
+    A_s = -CS1**(-1)*CS2
+    B_s = -CS1**(-1)*CS3
+    C_s = np.matrix([ [1,0,0,0 ],[0,1,0,0 ], [ 0,0,1,0 ], [ 0,0,0,1 ]])
+    D_s = np.matrix( [[0],[0],[0],[0]])
+    
+    sys_sym = control.StateSpace(A_s,B_s,C_s,D_s)
+    
+    
+    #ASYMMETRIC FLIGHT CONDITIONS
+    
+    CA1 = np.matrix( [[(CYbdot-2*mub)*b/V,0,0,0],    [0,-b/2/V,0,0],[0,0,-4*mub*KX2*b*b/2/V/V,4*mub*KXZ*b*b/2/V/V],[Cnb*b/V,0,4*mub*KXZ*b*b/2/V/V,-4*mub*KZ2*b*b/2/V/V] ])
+    CA2 = np.matrix([ [CYb,Cl,CYp*b/V/2,(CYr-4*mub)*b/2/V ],[0, 0, b/V/2 , 0 ], [ Clb,0,Clp*b/2/V,Clr*b/2/V ], [ Cnb,0,Cnp*b/2/V,Cnr*b/2/V ]])
+    CA3 = np.matrix( [[CYda,CYdr],[0,0],[Clda,Cldr],[Cnda,Cndr]])
+    
+    A_a = -CA1**(-1)*CA2
+    B_a = -CA1**(-1)*CA3
+    C_a = np.matrix([ [1,0,0,0 ],[0,1,0,0 ], [ 0,0,1,0 ], [ 0,0,0,1 ]])
+    D_a = np.matrix( [[0,0],[0,0],[0,0],[0,0]])
+    
+    sys_asym = control.StateSpace(A_a,B_a,C_a,D_a)
+    
+    return sys_sym, sys_asym
+    
+    
